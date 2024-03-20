23f56d65323ffa1ad6f8e097055066f0ea883b71
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-20 10:11:58 +0300

    User and role classes created and Connectionstring wrote

---
+++ IdentityMessage/Models/AppRole.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityMessage.Models;
+
+public class AppRole : IdentityRole
+{
+}
---
+++ IdentityMessage/Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityMessage.Models;
+
+public class AppUser : IdentityUser
+{
+
+    public string Name { get; set; }
+    public string Surname { get; set; }
+}
