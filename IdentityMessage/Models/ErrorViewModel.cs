47a911f32e68d7c4b936ebc60266ff15632ac348
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-20 09:55:32 +0300

    Add project files.

---
+++ IdentityMessage/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IdentityMessage.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
