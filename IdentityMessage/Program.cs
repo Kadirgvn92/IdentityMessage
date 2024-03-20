01058c55475002d3f7e7e8dac6439742acbb626b
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-20 10:21:04 +0300

    First migration has been done

--- IdentityMessage/Program.cs
+++ IdentityMessage/Program.cs
@@ -1,7 +1,29 @@
+using IdentityMessage.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllersWithViews();
+
+
+//burada appsettings tarafýna yazdýðýmýz sqlcon connectionstringi bu þekilde tanýmladýk
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon")); 
+});
+
+//Identity için aþaðýdaki þekilde tanýmlamalarýmýzý yaptýk
+builder.Services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<AppDbContext>(); 
+
+
+
+
+
+
+
+
+
 
 var app = builder.Build();
 
