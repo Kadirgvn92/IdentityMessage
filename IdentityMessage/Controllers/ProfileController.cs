0ed887a9d804291207ff67b5e0c73fe9b70c2b37
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-26 16:14:25 +0300

    Profile updated

--- IdentityMessage/Controllers/ProfileController.cs
+++ IdentityMessage/Controllers/ProfileController.cs
@@ -26,11 +26,11 @@ public class ProfileController : Controller
     [HttpGet]
     public IActionResult Index()
     {
-        
+
         return View();
     }
     [HttpPost]
-    public async Task<IActionResult> Index(ProfileViewModel model)
+    public async Task<IActionResult> Index(PasswordChangeViewModel model)
     {
         var hasUser = await _userManager.FindByNameAsync(User.Identity.Name);
 
@@ -39,16 +39,16 @@ public class ProfileController : Controller
             return View();
         }
 
-        var checkOldPassword = await _userManager.CheckPasswordAsync(hasUser, model.PasswordChangeViewModel.OldPassword); //burada eski şifreyi check yaptırıyoruz.
+        var checkOldPassword = await _userManager.CheckPasswordAsync(hasUser, model.OldPassword); //burada eski şifreyi check yaptırıyoruz.
 
-        if(!checkOldPassword)
+        if (!checkOldPassword)
         {
             ModelState.AddModelError(string.Empty, "Eski şifreniz yanlıştır.");
         }
 
-        var result = await _userManager.ChangePasswordAsync(hasUser, model.PasswordChangeViewModel.OldPassword, model.PasswordChangeViewModel.NewPassword);//Yeni şifreyle dğeiştiriyoruz.
+        var result = await _userManager.ChangePasswordAsync(hasUser, model.OldPassword, model.NewPassword);//Yeni şifreyle dğeiştiriyoruz.
 
-        if(!result.Succeeded)
+        if (!result.Succeeded)
         {
             foreach (IdentityError item in result.Errors)
             {
@@ -59,51 +59,13 @@ public class ProfileController : Controller
 
         await _userManager.UpdateSecurityStampAsync(hasUser); //nurada şifremizi dğeiştirdikten sonra security stamp güncelledik.
         await _signInManager.SignOutAsync(); //kulllanıcıya yeni şifresiyle girmesi için çıkış yaptırdık 
-        await _signInManager.PasswordSignInAsync(hasUser, model.PasswordChangeViewModel.NewPassword, true, false); //kullanıcıya yeni şifresiyle sisteme giriş yaptırdık.
+        await _signInManager.PasswordSignInAsync(hasUser, model.NewPassword, true, false); //kullanıcıya yeni şifresiyle sisteme giriş yaptırdık.
 
         TempData["PasswordChange"] = "Şifreniz başarıyla değiştirilmiştir.";
 
-        ViewBag.Gender = new SelectList(Enum.GetNames(typeof(Gender))); 
 
-        UserEditViewModel userEditViewModel = new UserEditViewModel();
-        userEditViewModel.Username = hasUser.UserName;
-        userEditViewModel.Email = hasUser.Email;
-        userEditViewModel.Phone = hasUser.PhoneNumber;
-        userEditViewModel.BirthDate = hasUser.BirthDate;
-        userEditViewModel.Gender = hasUser.Gender;
-        return View(userEditViewModel);
-
-    }
-    [HttpPost]
-    public async Task<IActionResult> UserEdit(ProfileViewModel model)   
-    {
-        var hasUser = await _userManager.FindByNameAsync(User.Identity.Name);
-
-        if(model.UserEditViewModel.ImageUrl != null)
-        {
-            var resource = Directory.GetCurrentDirectory();
-            var extension = Path.GetExtension(model.UserEditViewModel.Image.FileName);
-            var imagename = Guid.NewGuid() + extension;
-            var savelocation = resource + "/wwwroot/userimages/" + imagename;
-            var stream = new FileStream(savelocation, FileMode.Create);
-            await model.UserEditViewModel.Image.CopyToAsync(stream);
-            model.UserEditViewModel.ImageUrl = imagename;
-        }
-
-        hasUser.Email = model.UserEditViewModel.Email;
-        hasUser.UserName = model.UserEditViewModel.Username;
-        hasUser.PhoneNumber = model.UserEditViewModel.Phone;
-        hasUser.BirthDate = model.UserEditViewModel.BirthDate;
-        hasUser.ImageUrl = model.UserEditViewModel.ImageUrl;
-
-        //burada kaldım
-
-
-        if (!ModelState.IsValid)
-        {
-            return View();
-        }
         return View();
 
     }
+
 }
